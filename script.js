40efcd28bbc985f28ef6a3f7a659784d4500a874
sanatab <120128180+sanatab@users.noreply.github.com>
2023-01-23 01:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+var reversed = str.split("").reverse().join("");
+	if(revered === str){
+		return true;
+	}
+	else{
+		return false;
+}
 }
 module.exports = palindrome
